21e813c76b8a463b84138165a844b481f5527aae
Niklas Lindholm <niklas@nilin.se>
2015-09-04 19:46:49 -0700

    Revamp of InitNiKom. Updated to create ConferenceTexts.dat and Users/0/0/UnreadTexts instead of the old files.
    Also changed to not use nikom.library since this is dangerous.
    nikom.library should only be called when there is a server running.
    
    Lastly, changed to delete much much more of any existing old
    files before creating new files.

--- InitNiKom/InitNiKom.c
+++ InitNiKom/InitNiKom.c
@@ -5,140 +5,221 @@
 #include <proto/dos.h>
 #include <dos.h>
 #include <dos/dos.h>
+#include <dos/exall.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
 #include "NiKomStr.h"
 
-#include "NiKomLib.h"
-/*
-#include "/nikomlib/nikom_pragmas.h"
-#include "/nikomlib/nikom_protos.h"
-*/
-void ClearFiles(void);
+int deleteDir(char *dirname);
 
 struct Library *NiKomBase;
 
-struct SysInfo info;
+struct User userData = {
+	0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,
+	"Sysop","","","","","","sysop",
+	100,25,0,"-->","","",""
+};
+struct SysInfo sysInfo;
+struct UnreadTexts unreadTexts;
 
-void init(void)
-{
-	FILE *fp;
-	int ret, x;
-	short foo;
-
-	memset((void *)&info, 0, sizeof(struct SysInfo));
-
-	ClearFiles();
-
-	if(NiKomBase = OpenLibrary("nikom.library",19))
-	{
-		ret = CreateUserTags(-1,
-								US_Name, "sysop",
-								US_Street, "",
-								US_Address, "",
-								US_Country, "",
-								US_OtherInfo, "",
-								US_Phonenumber, "",
-								US_Password, "sysop",
-								US_Prompt, "-->",
-								US_Charset, (byte)0,
-								US_Status, (byte)100,
-								US_Rader, (byte)24,
-								US_Flags, (byte)2047,
-								TAG_END);
-
-		printf("Returvärde = %d\n", ret);
-		CloseLibrary(NiKomBase);
-		if(ret < 0) return;
-	}
-	else
-	{
-		printf("Kunde inte öppna NiKom.library!\n");
-		return;
-	}
-
-	if(fp=fopen("NiKom:DatoCfg/Sysinfo.dat","w"))
-	{
-		fwrite((void *)&info,sizeof(struct SysInfo),1,fp);
-		fclose(fp);
-	}
-	else
-		printf("Kunde inte öppna NiKom:DatoCfg/Sysinfo.dat\n");
-
-	if(fp=fopen("NiKom:DatoCfg/Möten.dat","w"))
-		fclose(fp);
-	else
-		printf("Kunde inte öppna NiKom:DatoCfg/Möten.dat\n");
-
-/*	for(x=0;x<MAXTEXTS;x++)
-		foo[x]=~0; */
-
-	if(fp=fopen("NiKom:DatoCfg/Textmot.dat","w"))
-	{
-		foo = ~0;
-		for(x=0;x<MAXTEXTS;x++)
-		{
-			if(fwrite((void *)&foo,2,1,fp)!=1)
-			{
-				printf("Kunde inte skriva till TextMot.dat\n");
-				break;
-			}
-		}
-		fclose(fp);
-	}
-	else
-		printf("Kunde inte öppna NiKom:DatoCfg/Textmot.dat\n");
-
-	if(fp=fopen("NiKom:DatoCfg/Areor.dat","w"))
-		fclose(fp);
-	else
-		printf("Kunde inte öppna NiKom:DatoCfg/Areor.dat\n");
-
-	if(fp=fopen("NiKom:DatoCfg/Grupper.dat","w"))
-		fclose(fp);
-	else
-		printf("Kunde inte öppna NiKom:DatoCfg/Grupper.dat\n");
+void createDirOrDie(char *dirname) {
+  BPTR lock;
+  struct FileInfoBlock __aligned fib;
+  
+  if(lock = Lock(dirname, ACCESS_READ)) {
+    if(!Examine(lock, &fib)) {
+      printf("Could not examine %s\n", dirname);
+      UnLock(lock);
+      exit(10);
+    }
+    UnLock(lock);
+    if(fib.fib_DirEntryType <= 0) {
+      printf("Can't create dir %s, there is a file with the same name.\n", dirname);
+      exit(10);
+    }
+    return;
+  }
+  if(!(lock=CreateDir(dirname))) {
+    printf("Couldn't create directory %s\n", dirname);
+    exit(10);
+  }
+  UnLock(lock);
+  printf("Created directory %s\n", dirname);
 }
 
-int main(int argc, char **argv)
-{
-	char input[20];
-	int count=0;
-
-	if(argc != 2)
-	{
-		printf("Detta program kommer att rensa följande filer\n");
-		printf("Users/0/0\n");
-		printf("Sysinfo.dat\n");
-		printf("Möten.dat\n");
-		printf("Textmot.dat\n");
-		printf("Areor.dat\n");
-		printf("Grupper.dat\n\n");
-		printf("Är detta OK? (j/N) ");
-		while((input[count++]=getchar())!='\n');
-		if(input[0]=='j' || input[0]=='J') {
-			init();
-		} else printf("Nehepp!\n");
-	}
-	else
-	{
-		printf("Skapar lite filer för att NiKom ska gå att starta upp, var god vänta...   ");
-		fflush(stdout);
-		init();
-		printf("klart!\n");
-	}
-
-	return 0;
+void createFile(char *filename, void *data, int size) {
+  FILE *fp;
+  if(fp=fopen(filename, "w")) {
+    if(size > 0) {
+      fwrite(data, size, 1, fp);
+    }
+    fclose(fp);
+  } else {
+    printf("Could not write %s\n", filename);
+    exit(10);
+  }
+  printf("Created file %s\n", filename);
 }
 
-void ClearFiles(void)
-{
-	DeleteFile("NiKom:DatoCfg/Sysinfo.dat");
-	DeleteFile("NiKom:Users/0/0/Data");
-	DeleteFile("NiKom:Users/0/0/Bitmap0");
-	DeleteFile("NiKom:DatoCfg/Grupper.dat");
-	DeleteFile("NiKom:DatoCfg/Areor.dat");
-	DeleteFile("NiKom:DatoCfg/Textmot.dat");
+void createEmptyFile(char *filename) {
+  createFile(filename, NULL, 0);
 }
+
+void initSysop(void) {
+  long tid;
+
+  createDirOrDie("NiKom:Users");
+  createDirOrDie("NiKom:Users/0");
+  createDirOrDie("NiKom:Users/0/0");
+
+  time(&tid);
+  userData.forst_in=tid;
+  userData.senast_in=tid;
+  createFile("NiKom:Users/0/0/Data", &userData, sizeof(struct User));
+
+  unreadTexts.bitmapStartText = 0;
+  memset(unreadTexts.bitmap, 0xff, UNREADTEXTS_BITMAPSIZE/8);
+  memset(unreadTexts.lowestPossibleUnreadText, 0, MAXMOTE * sizeof(long));
+  createFile("NiKom:Users/0/0/UnreadTexts", &unreadTexts, sizeof(struct UnreadTexts));
+
+  createFile("Nikom:Users/0/0/.firstletter", "0", 1);
+  createFile("Nikom:Users/0/0/.nextletter", "0", 1);
+}
+
+void initSysInfo(void) {
+  memset(&sysInfo, 0, sizeof(struct SysInfo));
+  createFile("NiKom:DatoCfg/Sysinfo.dat", &sysInfo, sizeof(struct SysInfo));
+}
+
+void initFiles(void) {
+  initSysop();
+  createDirOrDie("NiKom:DatoCfg");
+  createDirOrDie("NiKom:Moten");
+  initSysInfo();
+  createEmptyFile("NiKom:DatoCfg/Möten.dat");
+  createEmptyFile("NiKom:DatoCfg/ConferenceTexts.dat");
+  createEmptyFile("NiKom:DatoCfg/Areor.dat");
+  createEmptyFile("NiKom:DatoCfg/Grupper.dat");
+}
+
+int deleteFile(char *filename) {
+  BPTR lock;
+
+  if(!(lock = Lock(filename, ACCESS_READ))) {
+    // Can't get lock, assume it doesn't exist.
+    return 1;
+  }
+  UnLock(lock);
+  if(!DeleteFile(filename)) {
+    printf("Couldn't delete file/dir '%s'\n", filename);
+    return 0;
+  }
+  printf("Deleted file/dir '%s'\n", filename);
+  return 1;
+}
+
+void deleteFileOrDie(char *filename) {
+  if(!deleteFile(filename)) {
+    exit(10);
+  }
+}
+
+int handleExAllResult(struct ExAllControl *ec, struct ExAllData *ed, char *dirname) {
+  char filename[100];
+  int success;
+  if(ec->eac_Entries == 0) {
+    return 1;
+  }
+  while(ed != NULL) {
+    sprintf(filename, "%s/%s", dirname, ed->ed_Name);
+    if(ed->ed_Type < 0) {
+      success = deleteFile(filename);
+    } else {
+      success = deleteDir(filename);
+    }
+    if(!success) {
+      return 0;
+    }
+    ed=ed->ed_Next;
+  }
+  return 1;
+}
+
+int deleteDir(char *dirname) {
+  char __aligned buffer[2048];
+  struct ExAllControl *ec;
+  int moreFromExAll, success = TRUE;
+  BPTR lock;
+
+  printf("Deleting all files in directory %s\n", dirname);
+  if(!(lock = Lock(dirname, ACCESS_READ))) {
+    printf("Can't lock directory %s\n", dirname);
+    return 0;
+  }
+  ec = (struct ExAllControl *) AllocDosObject(DOS_EXALLCONTROL, NULL);
+  ec->eac_LastKey=0L;
+  ec->eac_MatchString=NULL;
+  ec->eac_MatchFunc=NULL;
+  do {
+    moreFromExAll = ExAll(lock, (struct ExAllData *)buffer, 2048, ED_TYPE, ec);
+    if(!handleExAllResult(ec, (struct ExAllData *) buffer, dirname)) {
+      success = FALSE;
+      break;
+    }
+  } while(moreFromExAll);
+  
+  UnLock(lock);
+  FreeDosObject(DOS_EXALLCONTROL,ec);
+  if(success) {
+    success = deleteFile(dirname);
+  }
+  return success;
+}
+
+void deleteDirOrDie(char *dirname) {
+  if(!deleteDir(dirname)) {
+    exit(10);
+  }
+}
+
+void clearFiles(void) {
+  deleteDirOrDie("NiKom:Users");
+  deleteDirOrDie("NiKom:Moten");
+  deleteFileOrDie("NiKom:DatoCfg/Sysinfo.dat");
+  deleteFileOrDie("NiKom:DatoCfg/Möten.dat");
+  deleteFileOrDie("NiKom:DatoCfg/ConferenceTexts.dat");
+  deleteFileOrDie("NiKom:DatoCfg/Areor.dat");
+  deleteFileOrDie("NiKom:DatoCfg/Grupper.dat");
+}
+
+int main(int argc, char **argv) {
+  char input[20];
+  int count=0;
+
+  printf("The following files/dirs will be reset/deleted:\n");
+  printf(" Users\n");
+  printf(" Moten\n");
+  printf(" DatoCfg/Sysinfo.dat\n");
+  printf(" DatoCfg/Möten.dat\n");
+  printf(" DatoCfg/ConferenceTexts.dat\n");
+  printf(" DatoCfg/Areor.dat\n");
+  printf(" DatoCfg/Grupper.dat\n\n");
+  printf("Is this ok? (y/N) ");
+  while((input[count++] = getchar()) != '\n');
+  if(input[0] != 'y' && input[0] != 'Y') {
+    printf("Nope!\n");
+    return 0;
+  }
+
+  printf("Yep!\n\n");
+  printf("Creating files necessary for NiKom to start up, please wait...\n");
+  fflush(stdout);
+  clearFiles();
+  initFiles();
+  printf("Done!\n");
+
+  return 0;
+}
+
