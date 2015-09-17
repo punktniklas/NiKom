0d7e2a522a44018699dfa552e80511e2bf7e9f1a
Niklas Lindholm <niklas@nilin.se>
2015-09-16 18:53:46 -0700

    Stops login if user data can't be read. Fixes #5.

--- Nodes/NiKomCon.c
+++ Nodes/NiKomCon.c
@@ -67,136 +67,139 @@ void cleanup(int kod,char *fel)
 }
 
 void main(int argc, char **argv) {
-	int going=TRUE,forsok=2,car=1;
-	long tid;
-	char tellstr[100],*tmppscreen, titel[80];
-	UBYTE tillftkn;
-	NewList((struct List *)&aliaslist);
-	NewList((struct List *)&edit_list);
-	if(!(IntuitionBase=(struct IntuitionBase *)OpenLibrary("intuition.library",0)))
-		cleanup(ERROR,"Kunde inte öppna intuition.library\n");
-	if(!(UtilityBase=OpenLibrary("utility.library",37L)))
-		cleanup(ERROR,"Kunde inte öppna utility.library\n");
-	if(!(NiKomBase=OpenLibrary("nikom.library",0L)))
-		cleanup(ERROR,"Kunde inte öppna nikom.linbrary");
-	getnodeconfig("NiKom:Datocfg/ConNode.cfg");
-	if(!(initnode(NODCON))) cleanup(ERROR,"Kunde inte registrera noden i Servern\n");
-	if(!(nikomnodeport = CreateMsgPort()))
-		cleanup(ERROR,"Kunde inte skapa NiKomNode-porten");
-	sprintf(nikomnodeportnamn,"NiKomNode%d",nodnr);
-	nikomnodeport->mp_Node.ln_Name = nikomnodeportnamn;
-	nikomnodeport->mp_Node.ln_Pri = 1;
-	AddPort(nikomnodeport);
-	sprintf(rexxportnamn,"NiKomRexx%d",nodnr);
-	if(!(rexxport=(struct MsgPort *)CreateMsgPort()))
-		cleanup(ERROR,"Kunde inte öppna RexxPort\n");
-	rexxport->mp_Node.ln_Name=rexxportnamn;
-	rexxport->mp_Node.ln_Pri=50;
-	AddPort(rexxport);
-	if(!(RexxSysBase=(struct RsxLib *)OpenLibrary("rexxsyslib.library",0L)))
-		cleanup(ERROR,"Kunde inte öppna rexxsyslib.library\n");
-	if(pubscreen[0]=='-') tmppscreen=NULL;
-	else tmppscreen=pubscreen;
-	if(!(NiKwind=(struct Window *)OpenWindowTags(NULL,WA_Left,xpos,
-																	  WA_Top,ypos,
-																	  WA_Width,xsize,
-																	  WA_Height,ysize,
-																	  WA_IDCMP,IDCMP_CLOSEWINDOW,
-																	  WA_MinWidth,50,
-																	  WA_MinHeight,10,
-																	  WA_MaxWidth,~0,
-																	  WA_MaxHeight,~0,
-																	  WA_SizeGadget,TRUE,
-																	  WA_SizeBBottom, TRUE,
-																	  WA_DragBar,TRUE,
-																	  WA_DepthGadget,TRUE,
-																	  WA_CloseGadget,TRUE,
-																	  WA_SimpleRefresh,TRUE,
-																	  WA_ScreenTitle,"NiKomCon",
-																	  WA_AutoAdjust,TRUE,
-																	  WA_PubScreenName,tmppscreen,
-																	  TAG_DONE)))
-		cleanup(ERROR,"Kunde inte öppna fönstret\n");
-	if(!OpenIO(NiKwind)) cleanup(ERROR,"Kunde inte öppna IO\n");
-	if(!getkeyfile())
-		cleanup(ERROR,"Korrupt nyckelfil\n");
-	strcpy(Servermem->nodid[nodnr],nodid);
-	sprintf(titel,"Nod #%d CON: <Ingen inloggad>",nodnr);
-	SetWindowTitles(NiKwind,titel,(UBYTE *)-1L);
-	Servermem->connectbps[nodnr] = -1;
-	conreqtkn();
-do
-{
-	memset(commandhistory,0,1024);
-	Servermem->inne[nodnr].rader=0;
-	sendfile("NiKom:Texter/Inlogg.txt");
-	if(Servermem->cfg.ar.preinlogg) sendrexx(Servermem->cfg.ar.preinlogg);
-	going=TRUE;
-	while(going) {
-		puttekn("\r\nNamn: ",-1);
-		getstring(EKO,40,NULL);
-		if(!stricmp(inmat,Servermem->cfg.ny))
-		{
-/*			if(!reggadnamn[0] && ((struct ShortUser *)Servermem->user_list.mlh_TailPred)->nummer >= 4)
-				puttekn("\n\n\rDenna demoversion av NiKom kan hantera maximalt 5 användare.\n\r",-1);
-			else { Versionen är nu obegränsad 970930! */
-				if((car=nyanv())==2) goto panik;
-				going=FALSE;
-/*			} */
-		}
-		else if((inloggad=parsenamn(inmat))>=0) {
-			readuser(inloggad,&Servermem->inne[nodnr]);
-			forsok=2;
-			while(forsok) {
-				puttekn("\r\nLösen: ",-1);
-				if(Servermem->inne[nodnr].flaggor & STAREKOFLAG)
-					getstring(STAREKO,15,NULL);
-				else
-					getstring(EJEKO,15,NULL);
-				if(CheckPassword(inmat, Servermem->inne[nodnr].losen))
-				{
-					forsok=FALSE;
-					going=FALSE;
-				} else forsok--;
-			}
-		} else if(inloggad==-1) puttekn("\r\nHittar ej namnet\r\n",-1);
-	}
-	sprintf(titel,"Nod #%d CON: %s #%d",nodnr,Servermem->inne[nodnr].namn,inloggad);
-	SetWindowTitles(NiKwind,titel,(UBYTE *)-1L);
-        if(!ReadUnreadTexts(&Servermem->unreadTexts[nodnr], inloggad)) {
-          printf("Error reading unread text info for user %d\n", inloggad);
+  int going=TRUE,forsok=2,car=1;
+  long tid;
+  char tellstr[100],*tmppscreen, titel[80];
+  UBYTE tillftkn;
+  NewList((struct List *)&aliaslist);
+  NewList((struct List *)&edit_list);
+  if(!(IntuitionBase=(struct IntuitionBase *)OpenLibrary("intuition.library",0)))
+    cleanup(ERROR,"Kunde inte öppna intuition.library\n");
+  if(!(UtilityBase=OpenLibrary("utility.library",37L)))
+    cleanup(ERROR,"Kunde inte öppna utility.library\n");
+  if(!(NiKomBase=OpenLibrary("nikom.library",0L)))
+    cleanup(ERROR,"Kunde inte öppna nikom.linbrary");
+  getnodeconfig("NiKom:Datocfg/ConNode.cfg");
+  if(!(initnode(NODCON))) cleanup(ERROR,"Kunde inte registrera noden i Servern\n");
+  if(!(nikomnodeport = CreateMsgPort()))
+    cleanup(ERROR,"Kunde inte skapa NiKomNode-porten");
+  sprintf(nikomnodeportnamn,"NiKomNode%d",nodnr);
+  nikomnodeport->mp_Node.ln_Name = nikomnodeportnamn;
+  nikomnodeport->mp_Node.ln_Pri = 1;
+  AddPort(nikomnodeport);
+  sprintf(rexxportnamn,"NiKomRexx%d",nodnr);
+  if(!(rexxport=(struct MsgPort *)CreateMsgPort()))
+    cleanup(ERROR,"Kunde inte öppna RexxPort\n");
+  rexxport->mp_Node.ln_Name=rexxportnamn;
+  rexxport->mp_Node.ln_Pri=50;
+  AddPort(rexxport);
+  if(!(RexxSysBase=(struct RsxLib *)OpenLibrary("rexxsyslib.library",0L)))
+    cleanup(ERROR,"Kunde inte öppna rexxsyslib.library\n");
+  if(pubscreen[0]=='-') tmppscreen=NULL;
+  else tmppscreen=pubscreen;
+  if(!(NiKwind=(struct Window *)OpenWindowTags(NULL,WA_Left,xpos,
+                                               WA_Top,ypos,
+                                               WA_Width,xsize,
+                                               WA_Height,ysize,
+                                               WA_IDCMP,IDCMP_CLOSEWINDOW,
+                                               WA_MinWidth,50,
+                                               WA_MinHeight,10,
+                                               WA_MaxWidth,~0,
+                                               WA_MaxHeight,~0,
+                                               WA_SizeGadget,TRUE,
+                                               WA_SizeBBottom, TRUE,
+                                               WA_DragBar,TRUE,
+                                               WA_DepthGadget,TRUE,
+                                               WA_CloseGadget,TRUE,
+                                               WA_SimpleRefresh,TRUE,
+                                               WA_ScreenTitle,"NiKomCon",
+                                               WA_AutoAdjust,TRUE,
+                                               WA_PubScreenName,tmppscreen,
+                                               TAG_DONE)))
+    cleanup(ERROR,"Kunde inte öppna fönstret\n");
+  if(!OpenIO(NiKwind)) cleanup(ERROR,"Kunde inte öppna IO\n");
+  if(!getkeyfile())
+    cleanup(ERROR,"Korrupt nyckelfil\n");
+  strcpy(Servermem->nodid[nodnr],nodid);
+  sprintf(titel,"Nod #%d CON: <Ingen inloggad>",nodnr);
+  SetWindowTitles(NiKwind,titel,(UBYTE *)-1L);
+  Servermem->connectbps[nodnr] = -1;
+  conreqtkn();
+  do {
+    memset(commandhistory,0,1024);
+    Servermem->inne[nodnr].rader=0;
+    sendfile("NiKom:Texter/Inlogg.txt");
+    if(Servermem->cfg.ar.preinlogg) sendrexx(Servermem->cfg.ar.preinlogg);
+    going=TRUE;
+    while(going) {
+      puttekn("\r\nNamn: ",-1);
+      getstring(EKO,40,NULL);
+      if(!stricmp(inmat,Servermem->cfg.ny)) {
+        if((car=nyanv())==2) goto panik;
+        going=FALSE;
+      }
+      else if((inloggad=parsenamn(inmat))>=0) {
+        if(readuser(inloggad,&Servermem->inne[nodnr])) {
+          puttekn("Error reading user data.\r\n", -1);
+          goto panik;
         }
-	Servermem->inloggad[nodnr]=inloggad;
-	Servermem->idletime[nodnr] = time(NULL);
-	if(getft("NiKom:Texter/Bulletin.txt")>Servermem->inne[nodnr].senast_in) sendfile("NiKom:Texter/Bulletin.txt");
-	connection();
-	if(Servermem->cfg.logmask & LOG_UTLOGG) {
-		sprintf(outbuffer,"%s loggar ut från nod %d",getusername(inloggad),nodnr);
-		logevent(outbuffer);
-	}
-	if(Servermem->say[nodnr]) displaysay();
-	if(Servermem->cfg.ar.utlogg) sendrexx(Servermem->cfg.ar.utlogg);
-	sendfile("NiKom:Texter/Utlogg.txt");
-	sprintf(titel,"Nod #%d CON: <Ingen inloggad>",nodnr);
-	SetWindowTitles(NiKwind,titel,(UBYTE *)-1L);
-	SaveProgramCategory(inloggad);
-	Servermem->inloggad[nodnr]=-1;
+        // TODO: Extract password loop. Should be identical to in PreNode/Ser.c
+        forsok=2;
+        while(forsok) {
+          puttekn("\r\nLösen: ",-1);
+          if(Servermem->inne[nodnr].flaggor & STAREKOFLAG)
+            getstring(STAREKO,15,NULL);
+          else
+            getstring(EJEKO,15,NULL);
+          if(CheckPassword(inmat, Servermem->inne[nodnr].losen)) {
+            forsok=FALSE;
+            going=FALSE;
+          } else {
+            forsok--;
+          }
+        }
+      } else if(inloggad==-1) puttekn("\r\nHittar ej namnet\r\n",-1);
+    }
+    sprintf(titel,"Nod #%d CON: %s #%d",nodnr,Servermem->inne[nodnr].namn,inloggad);
+    SetWindowTitles(NiKwind,titel,(UBYTE *)-1L);
+    if(!ReadUnreadTexts(&Servermem->unreadTexts[nodnr], inloggad)) {
+      puttekn("Error reading unread text info.\r\n", -1);
+      // TODO: Log error
+      goto panik;
+    }
+    Servermem->inloggad[nodnr]=inloggad;
+    Servermem->idletime[nodnr] = time(NULL);
+    if(getft("NiKom:Texter/Bulletin.txt")>Servermem->inne[nodnr].senast_in) sendfile("NiKom:Texter/Bulletin.txt");
 
-	Servermem->action[nodnr]=0;
-	time(&tid);
-	Servermem->inne[nodnr].senast_in=tid;
-	Servermem->inne[nodnr].tot_tid+=(tid-logintime);
-	Servermem->inne[nodnr].loggin++;
-	Servermem->info.inloggningar++;
-	Servermem->inne[nodnr].defarea=area2;
-	writeuser(inloggad,&Servermem->inne[nodnr]);
-        WriteUnreadTexts(&Servermem->unreadTexts[nodnr], inloggad);
-	writesenaste();
-	freealiasmem();
-	sprintf(tellstr,"loggade just ut från nod %d",nodnr);
-	tellallnodes(tellstr);
-panik:
-	puttekn("\r\n\nEn inloggning till? (J/n)",-1);
-	} while((tillftkn=gettekn())!='n' && tillftkn!='N');
-	cleanup(OK,"");
+    connection();
+
+    if(Servermem->cfg.logmask & LOG_UTLOGG) {
+      sprintf(outbuffer,"%s loggar ut från nod %d",getusername(inloggad),nodnr);
+      logevent(outbuffer);
+    }
+    if(Servermem->say[nodnr]) displaysay();
+    if(Servermem->cfg.ar.utlogg) sendrexx(Servermem->cfg.ar.utlogg);
+    sendfile("NiKom:Texter/Utlogg.txt");
+    sprintf(titel,"Nod #%d CON: <Ingen inloggad>",nodnr);
+    SetWindowTitles(NiKwind,titel,(UBYTE *)-1L);
+    SaveProgramCategory(inloggad);
+    Servermem->inloggad[nodnr]=-1;
+    
+    Servermem->action[nodnr]=0;
+    time(&tid);
+    Servermem->inne[nodnr].senast_in=tid;
+    Servermem->inne[nodnr].tot_tid+=(tid-logintime);
+    Servermem->inne[nodnr].loggin++;
+    Servermem->info.inloggningar++;
+    Servermem->inne[nodnr].defarea=area2;
+    writeuser(inloggad,&Servermem->inne[nodnr]);
+    WriteUnreadTexts(&Servermem->unreadTexts[nodnr], inloggad);
+    writesenaste();
+    freealiasmem();
+    sprintf(tellstr,"loggade just ut från nod %d",nodnr);
+    tellallnodes(tellstr);
+  panik:
+    puttekn("\r\n\nEn inloggning till? (J/n)",-1);
+  } while((tillftkn=gettekn())!='n' && tillftkn!='N');
+  cleanup(OK,"");
 }
--- Nodes/NiKomSer.c
+++ Nodes/NiKomSer.c
@@ -70,120 +70,125 @@ void cleanup(int kod,char *text)
 }
 
 void main(int argc,char *argv[]) {
-	int x;
-	long tid;
-	char tellstr[100],*tmppscreen, titel[80], configname[50] = "NiKom:DatoCfg/SerNode.cfg";
-	if(argc>1) for(x=1;x<argc;x++) {
-		if(argv[x][0]=='-') {
-			if(argv[x][1]=='B') dtespeed=atoi(&argv[x][2]);
-			else if(argv[x][1]=='N') nodnr = atoi(&argv[x][2]);
-		} else strcpy(configname,argv[x]);
-	}
-	if(nodnr==-1) {
-		printf("NiKomSer måste startas från prenoden.\n");
-		exit(10);
-	}
-	NewList((struct List *)&aliaslist);
-	NewList((struct List *)&edit_list);
-	if(!(IntuitionBase=(struct IntuitionBase *)OpenLibrary("intuition.library",0)))
-		cleanup(ERROR,"Kunde inte öppna intuition.library\n");
-	if(!(UtilityBase=OpenLibrary("utility.library",37L)))
-		cleanup(ERROR,"Kunde inte öppna utility.library\n");
-	if(!(NiKomBase=OpenLibrary("nikom.library",0L)))
-		cleanup(ERROR,"Kunde inte öppna nikom.library\n");
+  int x;
+  long tid;
+  char tellstr[100],*tmppscreen, titel[80], configname[50] = "NiKom:DatoCfg/SerNode.cfg";
+  if(argc>1) for(x=1;x<argc;x++) {
+      if(argv[x][0]=='-') {
+        if(argv[x][1]=='B') dtespeed=atoi(&argv[x][2]);
+        else if(argv[x][1]=='N') nodnr = atoi(&argv[x][2]);
+      } else strcpy(configname,argv[x]);
+    }
+  if(nodnr==-1) {
+    printf("NiKomSer måste startas från prenoden.\n");
+    exit(10);
+  }
+  NewList((struct List *)&aliaslist);
+  NewList((struct List *)&edit_list);
+  if(!(IntuitionBase=(struct IntuitionBase *)OpenLibrary("intuition.library",0)))
+    cleanup(ERROR,"Kunde inte öppna intuition.library\n");
+  if(!(UtilityBase=OpenLibrary("utility.library",37L)))
+    cleanup(ERROR,"Kunde inte öppna utility.library\n");
+  if(!(NiKomBase=OpenLibrary("nikom.library",0L)))
+    cleanup(ERROR,"Kunde inte öppna nikom.library\n");
 
-	initnode(NODSPAWNED);
-	if(!(nikomnodeport = CreateMsgPort()))
-		cleanup(ERROR,"Kunde inte skapa NiKomNode-porten");
-	sprintf(nikomnodeportnamn,"NiKomNode%d",nodnr);
-	nikomnodeport->mp_Node.ln_Name = nikomnodeportnamn;
-	nikomnodeport->mp_Node.ln_Pri = 1;
-	AddPort(nikomnodeport);
-	sprintf(rexxportnamn,"NiKomRexx%d",nodnr);
-	if(!(rexxport=(struct MsgPort *)CreateMsgPort()))
-		cleanup(ERROR,"Kunde inte öppna RexxPort\n");
-	rexxport->mp_Node.ln_Name=rexxportnamn;
-	rexxport->mp_Node.ln_Pri = 50;
-	AddPort(rexxport);
-	if(!(RexxSysBase=(struct RsxLib *)OpenLibrary("rexxsyslib.library",0L)))
-		cleanup(ERROR,"Kunde inte öppna rexxsyslib.library\n");
-	getnodeconfig(configname);
-	if(pubscreen[0]=='-') tmppscreen=NULL;
-	else tmppscreen=pubscreen;
-	if(!(NiKwind=(struct Window *)OpenWindowTags(NULL,WA_Left,xpos,
-																	  WA_Top,ypos,
-																	  WA_Width,xsize,
-																	  WA_Height,ysize,
-																	  WA_IDCMP,IDCMP_CLOSEWINDOW,
-																	  WA_MinWidth,50,
-																	  WA_MinHeight,10,
-																	  WA_MaxWidth,~0,
-																	  WA_MaxHeight,~0,
-																	  WA_SizeGadget,TRUE,
-																	  WA_SizeBBottom, TRUE,
-																	  WA_DragBar,TRUE,
-																	  WA_DepthGadget,TRUE,
-																	  WA_CloseGadget,TRUE,
-																	  WA_SimpleRefresh,TRUE,
-																	  WA_ScreenTitle,"NiKomSer",
-																	  WA_AutoAdjust,TRUE,
-																	  WA_PubScreenName,tmppscreen,
-																	  TAG_DONE)))
-		cleanup(ERROR,"Kunde inte öppna fönstret\n");
-	if(!getkeyfile())
-		cleanup(ERROR,"Korrupt nyckelfil\n");
-	if(!OpenIO(NiKwind)) cleanup(ERROR,"Kunde inte öppna IO\n");
-	inloggad=Servermem->inloggad[nodnr];
-	conreqtkn();
-	serreqtkn();
-	sprintf(titel,"Nod #%d SER: %s #%d",nodnr,Servermem->inne[nodnr].namn,inloggad);
-	SetWindowTitles(NiKwind,titel,(char *)-1L);
-        if(!ReadUnreadTexts(&Servermem->unreadTexts[nodnr], inloggad)) {
-          printf("Error reading unread text info for user %d\n", inloggad);
-        }
-	if(getft("NiKom:Texter/Bulletin.txt")>Servermem->inne[nodnr].senast_in) sendfile("NiKom:Texter/Bulletin.txt");
-	connection();
+  initnode(NODSPAWNED);
+  if(!(nikomnodeport = CreateMsgPort()))
+    cleanup(ERROR,"Kunde inte skapa NiKomNode-porten");
+  sprintf(nikomnodeportnamn,"NiKomNode%d",nodnr);
+  nikomnodeport->mp_Node.ln_Name = nikomnodeportnamn;
+  nikomnodeport->mp_Node.ln_Pri = 1;
+  AddPort(nikomnodeport);
+  sprintf(rexxportnamn,"NiKomRexx%d",nodnr);
+  if(!(rexxport=(struct MsgPort *)CreateMsgPort()))
+    cleanup(ERROR,"Kunde inte öppna RexxPort\n");
+  rexxport->mp_Node.ln_Name=rexxportnamn;
+  rexxport->mp_Node.ln_Pri = 50;
+  AddPort(rexxport);
+  if(!(RexxSysBase=(struct RsxLib *)OpenLibrary("rexxsyslib.library",0L)))
+    cleanup(ERROR,"Kunde inte öppna rexxsyslib.library\n");
+  getnodeconfig(configname);
+  if(pubscreen[0]=='-') tmppscreen=NULL;
+  else tmppscreen=pubscreen;
+  if(!(NiKwind=(struct Window *)OpenWindowTags(NULL,WA_Left,xpos,
+                                               WA_Top,ypos,
+                                               WA_Width,xsize,
+                                               WA_Height,ysize,
+                                               WA_IDCMP,IDCMP_CLOSEWINDOW,
+                                               WA_MinWidth,50,
+                                               WA_MinHeight,10,
+                                               WA_MaxWidth,~0,
+                                               WA_MaxHeight,~0,
+                                               WA_SizeGadget,TRUE,
+                                               WA_SizeBBottom, TRUE,
+                                               WA_DragBar,TRUE,
+                                               WA_DepthGadget,TRUE,
+                                               WA_CloseGadget,TRUE,
+                                               WA_SimpleRefresh,TRUE,
+                                               WA_ScreenTitle,"NiKomSer",
+                                               WA_AutoAdjust,TRUE,
+                                               WA_PubScreenName,tmppscreen,
+                                               TAG_DONE)))
+    cleanup(ERROR,"Kunde inte öppna fönstret\n");
+  if(!getkeyfile())
+    cleanup(ERROR,"Korrupt nyckelfil\n");
+  if(!OpenIO(NiKwind)) cleanup(ERROR,"Kunde inte öppna IO\n");
+  inloggad=Servermem->inloggad[nodnr];
+  conreqtkn();
+  serreqtkn();
+  sprintf(titel,"Nod #%d SER: %s #%d",nodnr,Servermem->inne[nodnr].namn,inloggad);
+  SetWindowTitles(NiKwind,titel,(char *)-1L);
+  if(!ReadUnreadTexts(&Servermem->unreadTexts[nodnr], inloggad)) {
+    puttekn("Error reading unread text info.\r\n", -1);
+    // TODO: Log error
+    cleanup(ERROR, "Error reading unread text info.\n");
+  }
+  if(getft("NiKom:Texter/Bulletin.txt")>Servermem->inne[nodnr].senast_in) {
+    sendfile("NiKom:Texter/Bulletin.txt");
+  }
 
-	if(nodestate & NIKSTATE_NOCARRIER) {
-		conputtekn("\nCarrier dropped\n",-1);
-		if(Servermem->cfg.logmask & LOG_CARDROPPED) {
-			sprintf(outbuffer,"%s släpper carriern (nod %d)",getusername(inloggad),nodnr);
-			logevent(outbuffer);
-		}
-		if(Servermem->cfg.ar.cardropped) sendrexx(Servermem->cfg.ar.cardropped);
-	} else {
-		if(nodestate & NIKSTATE_LOGOUT) {
-			nodestate &= ~NIKSTATE_LOGOUT;
-			puttekn("\n\n\r*** Automagisk utloggning ***\n\n\r",-1);
-		} else if(nodestate & NIKSTATE_INACTIVITY) {
-			nodestate &= ~NIKSTATE_INACTIVITY;
-			puttekn("\n\n\r*** Utloggning p.g.a inaktivitet ***\n\n\r",-1);
-		}
-		radcnt=-174711;
-		if(Servermem->say[nodnr]) displaysay();
-		if(Servermem->cfg.ar.utlogg) sendrexx(Servermem->cfg.ar.utlogg);
-		sendfile("NiKom:Texter/Utlogg.txt");
-	}
-	SaveProgramCategory(inloggad);
-	Servermem->inloggad[nodnr]=-1;
-	if(Servermem->cfg.logmask & LOG_UTLOGG) {
-		sprintf(outbuffer,"%s loggar ut från nod %d",getusername(inloggad),nodnr);
-		logevent(outbuffer);
-	}
-	Servermem->action[nodnr]=0;
-	time(&tid);
-	Servermem->inne[nodnr].senast_in=tid;
-	Servermem->inne[nodnr].tot_tid+=(tid-logintime);
-	Servermem->inne[nodnr].loggin++;
-	Servermem->info.inloggningar++;
-	Servermem->inne[nodnr].defarea=area2;
-	writeuser(inloggad,&Servermem->inne[nodnr]);
-        WriteUnreadTexts(&Servermem->unreadTexts[nodnr], inloggad);
-	writesenaste();
-	abortinactive();
-	freealiasmem();
-	sprintf(tellstr,"loggade just ut från nod %d",nodnr);
-	tellallnodes(tellstr);
-	cleanup(nodestate,"");
+  connection();
+
+  if(nodestate & NIKSTATE_NOCARRIER) {
+    conputtekn("\nCarrier dropped\n",-1);
+    if(Servermem->cfg.logmask & LOG_CARDROPPED) {
+      sprintf(outbuffer,"%s släpper carriern (nod %d)",getusername(inloggad),nodnr);
+      logevent(outbuffer);
+    }
+    if(Servermem->cfg.ar.cardropped) sendrexx(Servermem->cfg.ar.cardropped);
+  } else {
+    if(nodestate & NIKSTATE_LOGOUT) {
+      nodestate &= ~NIKSTATE_LOGOUT;
+      puttekn("\n\n\r*** Automagisk utloggning ***\n\n\r",-1);
+    } else if(nodestate & NIKSTATE_INACTIVITY) {
+      nodestate &= ~NIKSTATE_INACTIVITY;
+      puttekn("\n\n\r*** Utloggning p.g.a inaktivitet ***\n\n\r",-1);
+    }
+    radcnt=-174711;
+    if(Servermem->say[nodnr]) displaysay();
+    if(Servermem->cfg.ar.utlogg) sendrexx(Servermem->cfg.ar.utlogg);
+    sendfile("NiKom:Texter/Utlogg.txt");
+  }
+  SaveProgramCategory(inloggad);
+  Servermem->inloggad[nodnr]=-1;
+  if(Servermem->cfg.logmask & LOG_UTLOGG) {
+    sprintf(outbuffer,"%s loggar ut från nod %d",getusername(inloggad),nodnr);
+    logevent(outbuffer);
+  }
+  Servermem->action[nodnr]=0;
+  time(&tid);
+  Servermem->inne[nodnr].senast_in=tid;
+  Servermem->inne[nodnr].tot_tid+=(tid-logintime);
+  Servermem->inne[nodnr].loggin++;
+  Servermem->info.inloggningar++;
+  Servermem->inne[nodnr].defarea=area2;
+  writeuser(inloggad,&Servermem->inne[nodnr]);
+  WriteUnreadTexts(&Servermem->unreadTexts[nodnr], inloggad);
+  writesenaste();
+  abortinactive();
+  freealiasmem();
+  sprintf(tellstr,"loggade just ut från nod %d",nodnr);
+  tellallnodes(tellstr);
+  cleanup(nodestate,"");
 }
 
