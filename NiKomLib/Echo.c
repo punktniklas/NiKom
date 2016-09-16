0d5e3d4c786c397ceeff29982f071fef6884f7ce
Niklas Lindholm <niklas@nilin.se>
2016-09-15 22:53:56 -0700

    Added logging when rescanning/renumbering a Fido forum.

--- NiKomLib/Echo.c
+++ NiKomLib/Echo.c
@@ -14,60 +14,65 @@
 #include <stdio.h>
 
 #include "NiKomBase.h"
-#include "/Include/NiKomLib.h"
+#include "NiKomLib.h"
 #include "funcs.h"
 #include "Logging.h"
 
-void __saveds __asm LIBReScanFidoConf(register __a0 struct Mote *motpek,register __d0 int motnr, register __a6 struct NiKomBase *NiKomBase) {
-	struct ExAllData *ead;
-	struct ExAllControl *eac;
-	void *buffer;
-	BPTR lock;
-	int min=0,max=0,tmp,more;
-	struct Mote *newmotpek;
+void __saveds __asm LIBReScanFidoConf(register __a0 struct Mote *motpek,
+                                      register __d0 int motnr,
+                                      register __a6 struct NiKomBase *NiKomBase) {
+  struct ExAllData *ead;
+  struct ExAllControl *eac;
+  void *buffer;
+  BPTR lock;
+  int min=0,max=0,tmp,more;
+  struct Mote *newmotpek;
+  
+  if(!NiKomBase->Servermem) return;
 
-	if(!NiKomBase->Servermem) return;
-
-	if(motpek) newmotpek = motpek;
-	else {
-		newmotpek=LIBGetConfPoint(motnr,NiKomBase);
-		if(!newmotpek) return;
-	}
-	if(lock = Lock(newmotpek->dir,ACCESS_READ)) {
-		if(buffer = AllocMem(2048,MEMF_CLEAR)) {
-			if(eac = AllocDosObject(DOS_EXALLCONTROL,NULL)) {
-				eac->eac_LastKey = 0;
-				eac->eac_MatchString = NULL;
-				eac->eac_MatchFunc = NULL;
-				do {
-					more=ExAll(lock,buffer,2048,ED_NAME,eac);
-					if(eac->eac_Entries == 0) continue;
-					ead = (struct ExAllData *)buffer;
-					while(ead) {
-						if(ead->ed_Name[0] >= '0' && ead->ed_Name[0] <= '9') {
-							tmp=atoi(ead->ed_Name);
-							if(tmp!=1) { /* Vi vill inte ha HWM */
-								if(!min || tmp<min) {
-									min=tmp;
-								}
-								if(tmp>max) {
-									max=tmp;
-								}
-							}
-						}
-						ead=ead->ed_Next;
-					}
-				} while(more);
-				FreeDosObject(DOS_EXALLCONTROL,eac);
-			}
-			FreeMem(buffer,2048);
-		}
-		UnLock(lock);
-	}
-	if(min<2) min=2;
-	if(!max) max=1;
-	newmotpek->lowtext = min + newmotpek->renumber_offset;
-	newmotpek->texter = max + newmotpek->renumber_offset;
+  if(motpek) newmotpek = motpek;
+  else {
+    newmotpek=LIBGetConfPoint(motnr,NiKomBase);
+    if(!newmotpek) return;
+  }
+  if(lock = Lock(newmotpek->dir,ACCESS_READ)) {
+    if(buffer = AllocMem(2048,MEMF_CLEAR)) {
+      if(eac = AllocDosObject(DOS_EXALLCONTROL,NULL)) {
+        eac->eac_LastKey = 0;
+        eac->eac_MatchString = NULL;
+        eac->eac_MatchFunc = NULL;
+        do {
+          more=ExAll(lock,buffer,2048,ED_NAME,eac);
+          if(eac->eac_Entries == 0) continue;
+          ead = (struct ExAllData *)buffer;
+          while(ead) {
+            if(ead->ed_Name[0] >= '0' && ead->ed_Name[0] <= '9') {
+              tmp=atoi(ead->ed_Name);
+              if(tmp!=1) { /* Vi vill inte ha HWM */
+                if(!min || tmp<min) {
+                  min=tmp;
+                }
+                if(tmp>max) {
+                  max=tmp;
+                }
+              }
+            }
+            ead=ead->ed_Next;
+          }
+        } while(more);
+        FreeDosObject(DOS_EXALLCONTROL,eac);
+      }
+      FreeMem(buffer,2048);
+    }
+    UnLock(lock);
+  }
+  if(min<2) min=2;
+  if(!max) max=1;
+  newmotpek->lowtext = min + newmotpek->renumber_offset;
+  newmotpek->texter = max + newmotpek->renumber_offset;
+  LogEvent(NiKomBase->Servermem, FIDO_LOG, INFO,
+           "Full scan of area %s. Low %d (%d.msg), high %d (%d.msg)",
+           newmotpek->tagnamn, newmotpek->lowtext, min, newmotpek->texter, max);
 }
 
 void __saveds __asm LIBUpdateFidoConf(register __a0 struct Mote *motpek,register __a6 struct NiKomBase *NiKomBase) {
@@ -156,38 +161,55 @@ void __saveds __asm LIBReScanAllFidoConf(register __a6 struct NiKomBase *NiKomBa
 *               hårddisken.
 */
 
-int __saveds __asm LIBReNumberConf(register __a0 struct Mote *motpek,register __d0 int motnr,
-		register __d1 int newlowest, register __a6 struct NiKomBase *NiKomBase) {
-	struct Mote *newmotpek;
-	int i, reallowest, realhighest,diff,hwm;
-	char oldpath[100],newpath[100],filename[20];
-
-	if(!NiKomBase->Servermem) return(-1);
-
-	if(motpek) newmotpek = motpek;
-	else {
-		newmotpek=LIBGetConfPoint(motnr,NiKomBase);
-		if(!newmotpek) return(1);
-	}
-	if(newmotpek->type != MOTE_FIDO) return(3);
-	reallowest = newmotpek->lowtext - newmotpek->renumber_offset;
-	realhighest = newmotpek->texter - newmotpek->renumber_offset;
-	if(newlowest >= reallowest) return(2);
-	diff = reallowest - newlowest;
-	for(i=reallowest; i<=realhighest; i++) {
-		sprintf(filename,"%d.msg",i);
-		strcpy(oldpath,newmotpek->dir);
-		AddPart(oldpath,filename,99);
-		sprintf(filename,"%d.msg",i-diff);
-		strcpy(newpath,newmotpek->dir);
-		AddPart(newpath,filename,99);
-		Rename(oldpath,newpath);
-	}
-	newmotpek->renumber_offset += diff;
-	LIBWriteConf(newmotpek,NiKomBase);
-	if(!(hwm = gethwm(newmotpek->dir, LITTLE_ENDIAN))) return(4);
-	if(!sethwm(newmotpek->dir, hwm - diff, LITTLE_ENDIAN)) return(5);
-	return(0);
+int __saveds __asm LIBReNumberConf(register __a0 struct Mote *conf,register __d0 int confId,
+                                   register __d1 int newlowest,
+                                   register __a6 struct NiKomBase *NiKomBase) {
+  struct Mote *confToRenum;
+  int i, reallowest, realhighest, diff, hwm;
+  char oldpath[100], newpath[100], filename[20];
+  
+  if(!NiKomBase->Servermem) {
+    return -1;
+  }
+  
+  if(conf) {
+    confToRenum = conf;
+  } else {
+    confToRenum = LIBGetConfPoint(confId, NiKomBase);
+    if(!confToRenum) {
+      return 1;
+    }
+  }
+  if(confToRenum->type != MOTE_FIDO) {
+    return 3;
+  }
+  reallowest = confToRenum->lowtext - confToRenum->renumber_offset;
+  realhighest = confToRenum->texter - confToRenum->renumber_offset;
+  if(newlowest >= reallowest) {
+    return 2;
+  }
+  diff = reallowest - newlowest;
+  for(i = reallowest; i <= realhighest; i++) {
+    sprintf(filename, "%d.msg", i);
+    strcpy(oldpath, confToRenum->dir);
+    AddPart(oldpath, filename, 99);
+    sprintf(filename, "%d.msg", i - diff);
+    strcpy(newpath, confToRenum->dir);
+    AddPart(newpath, filename, 99);
+    Rename(oldpath, newpath);
+  }
+  confToRenum->renumber_offset += diff;
+  LIBWriteConf(confToRenum, NiKomBase);
+  if(!(hwm = gethwm(confToRenum->dir, LITTLE_ENDIAN))) {
+    return 4;
+  }
+  if(!sethwm(confToRenum->dir, hwm - diff, LITTLE_ENDIAN)) {
+    return 5;
+  }
+  LogEvent(NiKomBase->Servermem, FIDO_LOG, INFO,
+           "Renumbered area %s. New lowest: %d.msg (%d lower than before)",
+           confToRenum->tagnamn, newlowest, diff);
+  return 0;
 }
 
 
