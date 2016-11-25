f639e4111ff956d303f1b92eab5808eb7da95cde
Niklas Lindholm <niklas@nilin.se>
2016-11-25 10:54:19 -0800

    Fixing bogus log warning when changing language to english.

--- Nodes/Languages.c
+++ Nodes/Languages.c
@@ -15,7 +15,7 @@ static char *localeLang[] = {
 };
 
 void LoadCatalogForUser(void) {
-  int lang = Servermem->inne[nodnr].language;
+  int lang = Servermem->inne[nodnr].language, err;
   
   CloseCatalog(g_Catalog);
   g_Catalog = NULL;
@@ -23,8 +23,15 @@ void LoadCatalogForUser(void) {
     LogEvent(SYSTEM_LOG, WARN, "User %d has invalid language setting (%d).", inloggad, lang);
     return;
   }
+  if(lang == 0) {
+    return;
+  }
   g_Catalog = OpenCatalog(NULL, "NiKom.catalog", OC_Language, localeLang[lang], TAG_DONE);
   if(g_Catalog == NULL) {
-    LogEvent(SYSTEM_LOG, WARN, "Could not load catalog for language %s.", localeLang[lang]);
+    err = IoErr();
+    if(err != 0) {
+      LogEvent(SYSTEM_LOG, WARN, "Could not load catalog for language %s (err = %d).",
+               localeLang[lang], err);
+    }
   }
 }
