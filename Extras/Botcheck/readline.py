58b16bd7fd45d713c8cffcd288bec211350959a4
patrikaxelsson <patrik.bo@gmail.com>
2017-03-29 05:32:53 +0200

    Botcheck limits (#82)
    * Fixed timeout so it also works when data is being continuously input.
    
    * Added option to limit how long lines to read.
    
    * Made botcheck use the new numChars limit readlines.py.

--- Extras/Botcheck/readline.py
+++ Extras/Botcheck/readline.py
@@ -7,22 +7,27 @@ import sys
 import select
 import optparse
 import os
+from datetime import datetime, timedelta
 
 parser = optparse.OptionParser(
     usage = "Usage: %prog [options]",
     description = "Read a line of text and print it",
     option_list = [
-        optparse.make_option("-t", "--timeout", type = "int", help = "Read timeout in seconds")])
+        optparse.make_option("-t", "--timeout", type = "int", help = "Timeout to read a line in seconds"),
+        optparse.make_option("-n", "--numChars", type = "int", help = "Maximum number of characters to read")
+    ])
 
 options, args = parser.parse_args()
 
+timeout = timedelta(seconds = options.timeout) if options.timeout else timedelta.max
+numChars = options.numChars if options.numChars else sys.maxsize
 returnValue = 0
-timeoutCount = 0
 chars = []
 
 unbufferedStdin = os.fdopen(sys.stdin.fileno(), "rb", 0)
 
-while True:
+startDatetime = datetime.now()
+while len(chars) < numChars:
     readable, writeable, exceptions = select.select([unbufferedStdin], [], [], 1)
     if unbufferedStdin in readable:
         timeoutCount = 0
@@ -31,11 +36,10 @@ while True:
             chars.append(c)
         else:
             break
-    else:
-        timeoutCount += 1
-        if options.timeout and options.timeout <= timeoutCount: 
-            returnValue = 128 # Same as bash read when it times out 
-            break
+
+    if datetime.now() - startDatetime > timeout:
+        returnValue = 128 # Same as bash read when it times out
+        break
 
 print "".join(chars) # Send the read string back on stdout
 sys.exit(returnValue)
