9ff9472705c5a44efae5497768fe1051fc6f0d1c
Marcus Sundberg <marcus@marcussundberg.com>
2017-04-24 19:46:32 +0200

    nik_fopen: Ensure we do not return uninitialized fh.

--- Nodes/NiKTransfer.c
+++ Nodes/NiKTransfer.c
@@ -56,7 +56,7 @@ extern int writefiles(int);
 extern int updatefile(int,struct Fil *);
 
 static long __saveds __regargs nik_fopen(char *filename,char *accessmode) {
-	BPTR fh;
+	BPTR fh = NULL;
 	switch(accessmode[0]) {
 		case 'r' :
 			fh=Open(filename,MODE_OLDFILE);
