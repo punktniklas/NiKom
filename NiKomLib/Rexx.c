a057fb6a7dea3e9e53f828c766d8522148046fe5
Marcus Sundberg <marcus@marcussundberg.com>
2017-05-01 22:38:05 +0200

    LIBRexxEntry: Implement clearing A1 for gcc as well.
    Also use putreg() defined in dos.h on SAS/C instead of
    __builtin_putreg().

--- NiKomLib/Rexx.c
+++ NiKomLib/Rexx.c
@@ -1,15 +1,21 @@
 #include <exec/types.h>
 #include <rexx/storage.h>
-#include <dos.h>
 #include <string.h>
 
 #include "Funcs.h"
+
+#ifdef __GNUC__
+# define clear_a1() asm("subal %a1,%a1")
+#else
+# include <dos.h>
+# define clear_a1() putreg(REG_A1, NULL);
+#endif
 
 /* Glöm inte att lägga till koll om Servermem finns! */
 
 LONG __saveds AASM LIBRexxEntry(register __a0 struct RexxMsg *mess AREG(a0),register __a6 struct NiKomBase *NiKomBase AREG(a6)) {
 	if(!stricmp(mess->rm_Args[0],"MATRIX2NIKOM")) LIBMatrix2NiKom(NiKomBase);
 	else return(1);
-	__builtin_putreg(REG_A1,NULL);
+	clear_a1();
 	return(0);
 }
