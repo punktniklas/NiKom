e97fae50f26d82e9cad4907066a7426b5b5378fa
patrikaxelsson <patrik.bo@gmail.com>
2017-08-11 04:11:36 +0200

    botcheck now strips telnet commands from the answer (#100)
    * botcheck now strips telnet commands from the answer
    
    * Fixed error in comparison for sub-option.
    
    * Made striptelnetcmds.py handle any number of lines for completeness.

---
+++ Extras/Botcheck/striptelnetcmds.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# This script strips telnet command data from stdin.
+
+import sys
+
+state = "Normal"
+
+for line in sys.stdin:
+    chars = []
+    for c in line:
+        if "Normal" == state:
+            if "\xff" == c:
+                state = "Command"
+            else:
+                chars.append(c)
+        elif "Command" == state:
+            command = c
+            if "\xff" == command:
+                # Command followed by command should output command
+                chars.append("\xff")
+                state = "Normal"
+            elif "\xfa" == command:
+                state = "SubOption"
+            elif command in ("\xfb", "\xfc", "\xfd", "\xfe"):
+                # WILL, WON'T, DO and DON'T is followed by a byte of option code
+                state = "OptionCode"
+            else:
+                state = "Normal"
+        elif "SubOption" == state:
+            # Bytes following a sub-option til next command are part of it
+            if "\xff" == c:
+                state = "Command"
+        elif "OptionCode" == state:
+            state = "Normal"
+    
+    sys.stdout.write("".join(chars))
+
